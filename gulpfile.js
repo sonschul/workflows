b01f41c953587623a12b4a648595af93bbbc6ae3
Sonja Schulze <sonschul@uos.de>
2018-08-23 13:00:57 +0200

    Added static monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp'),
     gutil = require ('gulp-util'),
     coffee = require ('gulp-coffee'), 
     concat = require ('gulp-concat'),
+    compass = require ('gulp-compass'),
+    less = require('gulp-less'),
+    livereload = require('gulp-livereload'),
+    path = require('path'),
     browserify = require('gulp-browserify');
     
-    
-
     
 var coffeeScripts = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -29,4 +31,48 @@ gulp.task('js', function(){
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+        livereload.listen();
 });
+
+gulp.task('compass', function() {
+  gulp.src('components/sass/style.scss')
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
+    livereload.listen();
+});
+
+gulp.task('less', function() {
+  gulp.src('less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('builds/development/'))
+    .pipe(livereload());
+});
+
+gulp.task('html', function(){
+    gulp.src('builds/development/*.html')
+    livereload.listen();
+});
+
+
+gulp.task('json', function(){
+    gulp.src('builds/development/*.json')
+    livereload.listen();
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('components/coffee/tagline.coffee',['coffee']);
+    gulp.watch('components/scripts/*.js',['js']);
+    gulp.watch('components/sass/*.scss',['compass']);
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('builds/development/*.html',['html']);
+    gulp.watch('builds/development/*.json',['json']);
+});
+
+
+gulp.task('default',['html','json','coffee','js','compass','less','watch']);
